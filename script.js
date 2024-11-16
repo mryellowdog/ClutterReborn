0ac621ce0bf2f275355c7f92938c6eccaf2085e2
yellowdog <84593165+mryellowdog@users.noreply.github.com>
2024-11-16 11:51:05 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,85 @@
+var plist = document.getElementById("project-list");
+async function get(url) {
+  let obj = await fetch(url);
+  let out = await obj.text();
+  let json = JSON.parse(out);
+  plist.innerHTML = plist.innerHTML + json.title + ", ";
+}
+//console.log(get("https://api.allorigins.win/raw?url=https://api.scratch.mit.edu/"))
+function getStringBetween(str, startStr, endStr) {
+  const startIndex = str.indexOf(startStr) + startStr.length;
+  const endIndex = str.indexOf(endStr, startIndex);
+  return str.substring(startIndex, endIndex);
+}
+
+var frame = document.getElementById("frame");
+var pre = document.getElementById("prev");
+var nex = document.getElementById("next");
+pre.style.visibility = "hidden";
+const list = [];
+var slide = 0;
+input = document.getElementById("input");
+function add() {
+  if (input.value.startsWith("https://scratch.mit.edu/projects")) {
+    list.push(input.value);
+    slide = 0;
+    frame.src = list[slide] + "embed";
+    if (list.length > 1) {
+      nex.style.visibility = "visible";
+    }
+    let id = getStringBetween(
+      input.value,
+      "https://scratch.mit.edu/projects/",
+      "/"
+    );
+    get(
+      "https://api.codetabs.com/v1/proxy/?quest=https://api.scratch.mit.edu/projects/" +
+        id
+    );
+    /*let json = get("https://api.codetabs.com/v1/proxy/?quest=https://api.scratch.mit.edu/projects/" + id);
+          console.log(json.PromiseResult);
+          let o = JSON.parse(json);
+          plist.innerHTML = plist.innerHTML + ", " + o.title["0"];
+          */
+  } else {
+    alert("You can only submit valid Scratch project links.");
+  }
+}
+
+if (list.length !== 0) {
+  frame.src = list[slide] + "embed";
+} else {
+  nex.style.visibility = "hidden";
+}
+function next() {
+  slide += 1;
+  if (slide == list.length - 1) {
+    slide = list.length - 1;
+    nex.style.visibility = "hidden";
+  }
+  frame.src = list[slide] + "embed";
+  if (slide == 0) {
+    pre.style.visibility = "hidden";
+    nex.style.visibility = "visible";
+  }
+  if (slide !== 0) {
+    pre.style.visibility = "visible";
+  }
+}
+function prev() {
+  slide -= 1;
+  if (slide == list.length - 1) {
+    slide = list.length - 1;
+    nex.style.visibility = "hidden";
+  } else {
+    nex.style.visibility = "visible";
+  }
+  frame.src = list[slide] + "embed";
+  if (slide == 0) {
+    pre.style.visibility = "hidden";
+    nex.style.visibility = "visible";
+  }
+  if (slide !== 0) {
+    pre.style.visibility = "visible";
+  }
+}
