c629281376414f1f5e20fffd5cb913d56f4f20dd
BigNate469 <174724510+BigNate469@users.noreply.github.com>
2024-11-17 15:12:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -67,11 +67,11 @@ async function add() {
       console.log(studioProjects[i]);
       get('https://trampoline.turbowarp.org/api/projects/' + studioProjects[i]);
       list.push('https://scratch.mit.edu/projects/' + studioProjects[i] + '/');
-      frame.src = list[i] + "embed";
       if (list.length > 1) {
         nex.style.visibility = "visible";
       }
     };
+    frame.src = list[0] + "embed";
     slide = 0;
   } else {
     console.warn('invalid URL', input.value);
